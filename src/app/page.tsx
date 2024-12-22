c92e178f7a56f9dd8416a5e65e7be2887897ef6b
Sumera Imran <159164615+iweb21@users.noreply.github.com>
2024-12-22 19:34:23 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,12 @@ import DressStyle from "@/Components/Dresstyle";
 import HappyCustomers from "@/Components/Happycustomer";
 import Hero from "@/Components/Hero";
 import LogoBar from "@/Components/Herobottom";
-import Header from "@/Components/Header";
 import Selling from "@/Components/Selling";
 
 export default function Home() {
   return (
   <>
   <Hero/>
-  <Header/>
   <LogoBar/>
   <div className="md:px-[100px] ">
   <Arrivals/>
